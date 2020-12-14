f9b7fbbbf6b7d37c082a75b3be57c8791d9f404d
KatarzynaSt <katarzyna.steczniewska.stud@pw.edu.pl>
2020-12-14 09:29:01 +0000

    v1

--- lab6.cpp
+++ lab6.cpp
@@ -1,6 +1,27 @@
+#include <algorithm>
 #include <iostream>
+#include <numeric>
+#include <random>
+#include <string>
+#include <type_traits>
+#include <vector>
 
+using namespace std;
 int main()
 {
-    puts("Hello, World!");
+    string l;
+    cin >> l;
+
+    auto it  = adjacent_find(l.begin(), l.end());
+    auto it1 = adjacent_find(l.begin(), l.end());
+
+    if (it == l.begin()) {
+        it1 = l.begin() + 1;
+        if (it1 = it) {
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }
 }
--- zal/foo.hpp
+++ zal/foo.hpp
@@ -4,13 +4,24 @@
 
 #include <algorithm>
 #include <list>
+#include <numeric>
 #include <vector>
+using namespace std;
 
-std::vector< char > foo(std::list< Human >& people)
+vector< char > foo(list< Human >& people)
 {
-    std::vector< char > ret_v(people.size());
+    vector< char > ret_v;
 
-    // Twoja implementacja tutaj
+    std::for_each(people.rbegin(), people.rend(), [](Human& i){
+        i.birthday();}
 
-    return ret_v;
+    std::transform(people.rbegin(), people.rend(), ret_v.begin(), [](Human& it)
+     if (it->isMonster() == true)
+     {
+        return 'n';
+     }
+     else {
+        return 'y'; }
+
+    return {ret_v};
 }

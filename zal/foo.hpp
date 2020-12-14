262a495496cdd5a24b726ef24f72850b35f12f6c
KatarzynaSt <katarzyna.steczniewska.stud@pw.edu.pl>
2020-12-14 09:36:49 +0000

    v2

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -12,16 +12,15 @@ vector< char > foo(list< Human >& people)
 {
     vector< char > ret_v;
 
-    std::for_each(people.rbegin(), people.rend(), [](Human& i){
-        i.birthday();}
+    std::for_each(people.rbegin(), people.rend(), [](Human& i) { i.birthday(); });
 
-    std::transform(people.rbegin(), people.rend(), ret_v.begin(), [](Human& it)
-     if (it->isMonster() == true)
-     {
-        return 'n';
-     }
-     else {
-        return 'y'; }
-
+    std::transform(people.rbegin(), people.rend(), ret_v.begin(), [](Human& it) {
+        if (it.isMonster() == true) {
+            return 'n';
+        }
+        else {
+            return 'y';
+        }
+    });
     return {ret_v};
 }
